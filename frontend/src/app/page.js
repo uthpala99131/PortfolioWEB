a95942c428699632879c0ba00057d58dfcd70176
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-03-11 23:54:11 +0530

    add frontend files

---
+++ frontend/src/app/page.js
@@ -0,0 +1,23 @@
+import Hero from "./compnents/Hero";
+import Navbar from "./compnents/Navbar";
+import AboutSection from "./compnents/AboutSection";
+import ProjectsSection from "./compnents/ProjectsSection";
+import EmailSection from "./compnents/EmailSection";
+import Footer from "./compnents/Footer";
+import AchievementsSection from "./compnents/AchievementsSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#000000]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <Hero />
+        <AchievementsSection />
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
