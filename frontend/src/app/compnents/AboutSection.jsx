59d8b70725f460524ed8552257df74fa48666121
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-03-12 14:09:03 +0530

    update aboutsection

--- frontend/src/app/compnents/AboutSection.jsx
+++ frontend/src/app/compnents/AboutSection.jsx
@@ -52,7 +52,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-1 px-4 xl:gap-16 sm:py-8 xl:px-16">
         <Image src="/images/b3.png" alt="image not found" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-900">About Me</h2>
