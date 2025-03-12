f7b90def74171c1a0b3a67f547d8b0b3730448e5
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-03-12 11:26:48 +0530

    add another image

--- frontend/src/app/compnents/AboutSection.jsx
+++ frontend/src/app/compnents/AboutSection.jsx
@@ -53,17 +53,19 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" alt="image not found" width={500} height={500} />
+        <Image src="/images/b3.png" alt="image not found" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg ">
-          Hi, I'm Uthpala Abeysinghe, an aspiring UI/UX Designer and Full-Stack Developer passionate about creating seamless digital experiences. I am currently an undergraduate student pursuing a BSc in Information Technology for Business and have successfully completed my Diploma with a Distinction Pass. I also have a strong academic background, having passed my A/Ls in the Physical Science stream.
+          <h2 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-900">About Me</h2>
+          <p className="text-base lg:text-lg text-justify">
+          Hi, I'm Uthpala Abeysinghe, an aspiring UI/UX Designer and Full-Stack Developer passionate about creating seamless digital experiences. 
+          I am currently an undergraduate student pursuing a BSc in Information Technology for Business and have successfully completed my Diploma with a Distinction Pass.
+           I also have a strong academic background, having passed my A/Ls in the Physical Science stream.
 
           With expertise in front-end and back-end development, I specialize in: <br />
 
-          Web Technologies: HTML, CSS, JavaScript, PHP, Bootstrap <br />
-          Frameworks & Libraries: MERN Stack (MongoDB, Express.js, React.js, Node.js) <br />
-          CMS & Platforms: WordPress <br />
+          <li>Web Technologies: HTML, CSS, JavaScript, PHP, Bootstrap</li>
+           <li>Frameworks & Libraries: MERN Stack (MongoDB, Express.js, React.js, Node.js)</li> 
+           <li>CMS & Platforms: WordPress</li>
           I am always eager to learn new technologies and take on innovative projects. Whether it's designing intuitive interfaces or developing dynamic websites, I strive to build solutions that are both visually appealing and functionally robust.
 
           Let's connect and create something amazing together!
