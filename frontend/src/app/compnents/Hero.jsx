8c9b2ff6e83e55d67ec9f83bacfcc232c754e3cd
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-03-12 11:26:25 +0530

    remove image

--- frontend/src/app/compnents/Hero.jsx
+++ frontend/src/app/compnents/Hero.jsx
@@ -62,15 +62,7 @@ const Hero = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/b3.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
+         
         </motion.div>
       </div>
     </section>
