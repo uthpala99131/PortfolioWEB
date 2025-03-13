108f61e3fc553a352385b1e7a8dfbf4b61fcc3ed
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-03-13 12:51:55 +0530

    update project section

--- frontend/src/app/compnents/ProjectsSection.jsx
+++ frontend/src/app/compnents/ProjectsSection.jsx
@@ -5,11 +5,13 @@ import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
 const projectsData = [
-  { id: 1, title: "React Portfolio Website", description: "Project 1 description", image: "/images/projects/1.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
-  { id: 2, title: "Photography Portfolio Website", description: "Project 2 description", image: "/images/projects/2.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
-  { id: 3, title: "E-commerce Application", description: "Project 3 description", image: "/images/projects/3.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
-  { id: 4, title: "Food Ordering Application", description: "Project 4 description", image: "/images/projects/4.png", tag: ["All", "Mobile"], gitUrl: "/", previewUrl: "/" },
+  { id: 1, title: "My Portfolio Website", description: "Project 1 description", image: "/images/projects/p.jpg", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
+  { id: 2, title: "Superfresh Supermarket Website wordpress", description: "Project 2 description", image: "/images/projects/s.jpg", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
+  { id: 3, title: "AI Chat Bot", description: "Project 3 description", image: "/images/projects/c.jpg", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
+  { id: 4, title: "Bassunnahe Website", description: "Project 4 description", image: "/images/projects/b.jpg", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
   { id: 5, title: "React Firebase Template", description: "Authentication and CRUD operations", image: "/images/projects/5.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
+  { id: 6, title: "Full-stack Roadmap", description: "Project 5 description", image: "/images/projects/6.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
+  { id: 6, title: "Full-stack Roadmap", description: "Project 5 description", image: "/images/projects/6.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
   { id: 6, title: "Full-stack Roadmap", description: "Project 5 description", image: "/images/projects/6.png", tag: ["All", "Web"], gitUrl: "/", previewUrl: "/" },
 ];
 
